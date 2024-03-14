54a7f5fa75b65bc72c642f253caf6eb0c558c80c
Gustavo Pereira <gustavodasilvapereira14@gmail.com>
2024-03-14 17:24:50 -0300

    themes

--- lua/duascaras/packer.lua
+++ lua/duascaras/packer.lua
@@ -4,42 +4,46 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use ({
-	  'shatur/neovim-ayu',
-	  as = 'ayu',
-	  config = function()
-		  vim.cmd('colorscheme ayu')
-	  end
-  })
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  -- LSP Support
-		  { 'neovim/nvim-lspconfig' },
-		  { 'williamboman/mason.nvim' },
-		  { 'williamboman/mason-lspconfig.nvim' },
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-		  -- Autocompletion
-		  { 'hrsh7th/nvim-cmp' },
-		  { 'hrsh7th/cmp-nvim-lsp' },
-		  { 'L3MON4D3/LuaSnip' },
-	  }
-    } 
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+        }
+    }
+
+    --Themes
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use({ 'folke/tokyonight.nvim', as = 'tokyonight' })
+    use({ 'bluz71/vim-moonfly-colors', as = 'moonfly' })
+    use({ 'shatur/neovim-ayu', as = 'ayu' })
 end)
-
