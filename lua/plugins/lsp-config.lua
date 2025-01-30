8ebaee82faf8a95399ef124c0139d469132aa167
duascaras <gustavodasilvapereira14@gmail.com>
2025-01-30 20:35:24 -0300

    added clangd to lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ast_grep" }
+        ensure_installed = { "lua_ls", "clangd" }
       })
     end
   },
