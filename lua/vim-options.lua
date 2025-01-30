c869f29f9d6b7eff7487fe978fc713527c22776b
duascaras <gustavodasilvapereira14@gmail.com>
2025-01-30 20:37:04 -0300

    added toggle term function/remaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -34,10 +34,10 @@ vim.opt.colorcolumn = "80"
 
 vim.opt.numberwidth = 5
 
-vim.cmd [[
+vim.cmd([[
   highlight LineNr guifg=bg guibg=NONE
   highlight CursorLineNr guifg=bg guibg=NONE
-]]
+]])
 
 vim.opt.colorcolumn = ""
 
@@ -60,7 +60,7 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
@@ -80,3 +80,33 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Term Toggle Function
+local term_buf = nil
+local term_win = nil
+
+function TermToggle(height)
+    if term_win and vim.api.nvim_win_is_valid(term_win) then
+        vim.cmd("hide")
+    else
+        vim.cmd("botright new")
+        local new_buf = vim.api.nvim_get_current_buf()
+        vim.cmd("resize " .. height)
+        if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+            vim.cmd("buffer " .. term_buf) -- go to terminal buffer
+            vim.cmd("bd " .. new_buf) -- cleanup new buffer
+        else
+            vim.cmd("terminal")
+            term_buf = vim.api.nvim_get_current_buf()
+            vim.wo.number = false
+            vim.wo.relativenumber = false
+            vim.wo.signcolumn = "no"
+        end
+    vim.cmd("startinsert!")
+    term_win = vim.api.nvim_get_current_win()
+    end
+end
+
+-- Term Toggle Keymap
+vim.keymap.set("n", "<C-]>", ":lua TermToggle(20)<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-]>", "<C-\\><C-n>:lua TermToggle(20)<CR>", { noremap = true, silent = true })
